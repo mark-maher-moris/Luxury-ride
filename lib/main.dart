ffa832da42757403ce4d44533e175f9c42892c89
Mark Maher <66829236+mark-maher-moris@users.noreply.github.com>
2025-03-27 03:58:41 +0200

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:myapp/draft/widgets/custom_google_map.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:myapp/resources/routes_manager.dart';
 import 'package:myapp/resources/theme_manager.dart';
-import 'package:myapp/view/auth/login_screen.dart';
-import 'package:myapp/view/change_language_view.dart';
 import 'package:myapp/view/home/home_view.dart';
-import 'package:myapp/view/splash/splash_view.dart';
 import 'firebase_options.dart';
 
 void main() async {
